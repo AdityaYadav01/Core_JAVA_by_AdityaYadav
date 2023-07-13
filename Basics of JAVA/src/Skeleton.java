d37f29c59429388c09eb3473d62084322f3e85ea
Aditya Yadav <Adityayadav44556@gmail.com>
2023-07-13 22:29:28 +0530

    Basics of Java is added

---
+++ Basics of JAVA/src/Skeleton.java
@@ -0,0 +1,24 @@
+                        // Skeleton of JAVA Program
+
+/* 1. Public - It is an access modifier which allow to access class from anywhere
+*  2. class - It is the name group of properties and function.
+*  3. Skeleton - It is the name of the class as well as name of the file. */
+
+public class Skeleton {
+    public static void main(String[] args){
+
+        /* 4. public - It is an access modifier which allow main method to accessible from anywhere.
+        *  5. static - It is a keyword which allow main method to run without using objects.
+        *  6. void - It is a keyword which is used when we don't want to return anything from method.
+        *  7. main - It is the name of method. It is also an entry point for java program, and execution start from here.
+        *  8. String[] args - It is a command line argument of string type array.  */
+
+        System.out.println("Hello World");
+
+        /* 9. System - It is a final class defined in java.lang packages.
+        *  10. out - It refers to the standard output stream.
+        * It is a variable of PrintStream type which is public and static member field of the system class.
+        * 11. println() - It is the method of PrintStream class. It prints argument passed to it and adds a new line. */
+
+    }
+}
