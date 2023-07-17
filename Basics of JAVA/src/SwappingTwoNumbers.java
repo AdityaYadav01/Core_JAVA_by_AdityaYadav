3665f279ab71e27e5543152702965fba2815d955
Aditya Yadav <Adityayadav44556@gmail.com>
2023-07-17 12:51:01 +0530

    Swapping of Two Numbers

--- Basics of JAVA/src/SwappingTwoNumbers.java
+++ Basics of JAVA/src/SwappingTwoNumbers.java
@@ -18,5 +18,15 @@ public class SwappingTwoNumbers {
 
         System.out.println("The Swapped Numbers are: " + num1 + " " + num2);
 
+        // Swapping Two Numbers using XOR Operation.
+        int a = 10;
+        int b = 20;
+
+        a = a^b;
+        b = a^b;
+        a = a^b;
+
+        System.out.println("Two Swapped Numbers are: " + a + " " + b);
+
     }
 }
