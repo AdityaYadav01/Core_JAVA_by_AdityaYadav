bd02d5e8b11cca889fb5f0dd96586f3cc6a3b609
Aditya Yadav <Adityayadav44556@gmail.com>
2023-07-25 20:11:05 +0530

    Conditional Statements in JAVA

---
+++ Conditional Statements and Loops in Java/src/SyntaxForConditionalStatements.java
@@ -0,0 +1,18 @@
+/* 1. Conditional Statements - These are the statements that are executed randomly and repeatedly.
+*  2. Conditional Statements are useful for writing better and complex program.
+*  3. These statements perform single time execution from top to bottom.
+*  4. These statements is used to perform tasks depending on whether a given condition is true or false.*/
+
+public class SyntaxForConditionalStatements {
+    public static void main(String[] args) {
+
+        /*Syntax:
+        *
+        * if(condition){
+        *       Statement;
+        * else{
+        *       Statement;
+        *      }
+        * }*/
+    }
+}
