92cf8e9d31193d3445eb16d5b9444128d6db3c0c
Aditya Yadav <Adityayadav44556@gmail.com>
2023-07-25 19:05:11 +0530

    Contional Statement file is added

---
+++ Conditional Statements and Loop/src/ConditionalStatements.java
@@ -0,0 +1,18 @@
+/* 1. Conditional Statements are executed randomly and repeatedly.
+*  2. They are useful for writing better and complex programs.
+*  3. These statements represent a single time execution from top to bottom.
+*  4. This statement is used to perform a task depending on whether a given condition is true or false*/
+
+public class ConditionalStatements {
+    public static void main(String[] args) {
+
+        /* Syntax for conditional statements are:
+        if(condition){
+            statement;
+        }
+        else{
+            statement;
+        }*/
+
+    }
+}
