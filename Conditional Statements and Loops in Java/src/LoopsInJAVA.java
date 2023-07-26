f3b8513418143e64972e30bcb392eb69684503e2
Aditya Yadav <Adityayadav44556@gmail.com>
2023-07-26 19:04:37 +0530

    Loops in JAVA

---
+++ Conditional Statements and Loops in Java/src/LoopsInJAVA.java
@@ -0,0 +1,27 @@
+// Loops are used to iterate a part of program several times.
+
+public class LoopsInJAVA {
+    public static void main(String[] args) {
+
+        /* 1. For Loop - It is generally used when we know how many times will loop iterate.
+        *     Syntax:
+        *             for(intialization, condition, increment/decrement){
+        *                                               //BODY
+        *                                               }
+        *
+        *  2. While Loop - It is generally used when we don't know how many times loop iterate.
+        *       Syntax:
+        *               while(condition){
+        *                            BODY
+        *                            increment/decrement;
+        *                   }
+        *
+        *  3. do while loop - It is used when we want to execute our statement at least one time.
+        *        Syntax:
+        *                do{
+        *                   code to be executed;
+        *                   update statement - increment/decrement
+        *                  } while(condition);
+        */
+    }
+}
