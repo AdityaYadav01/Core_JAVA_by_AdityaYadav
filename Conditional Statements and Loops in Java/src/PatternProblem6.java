4049de09186ba97b688adba2711797a00f0cdc17
Aditya Yadav <Adityayadav44556@gmail.com>
2023-07-29 21:01:17 +0530

    Pattern Problem 6 is added

---
+++ Conditional Statements and Loops in Java/src/PatternProblem6.java
@@ -0,0 +1,32 @@
+/*Print Pattern:
+*   * * * * *
+      * * * *
+        * * *
+          * *
+            *      */
+
+import java.util.Scanner;
+
+public class PatternProblem6 {
+    public static void main(String[] args) {
+
+            Scanner in = new Scanner(System.in);
+
+            System.out.print("Enter the number of rows: ");
+            int rows = in.nextInt();
+
+            for (int i = 1; i <= rows ; i++) {
+                for (int j = 1; j <= rows ; j++) {
+                    if (i<=j){
+                        System.out.print("* ");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+
+                }
+                System.out.println("");
+
+            }
+    }
+}
