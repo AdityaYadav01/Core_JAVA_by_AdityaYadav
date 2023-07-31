f407df2c6893ed8ce104771c95375a9240e43573
Aditya Yadav <Adityayadav44556@gmail.com>
2023-07-31 20:23:15 +0530

    Rotation of an Array

--- Arrays in Java/src/RotatingAnArray.java
+++ Arrays in Java/src/RotatingAnArray.java
@@ -1,5 +1,10 @@
 public class RotatingAnArray {
     public static void main(String[] args) {
+        int[] arr = {89, 78, 96, 4, 5, 12, 13, 166, 61, 47, 866};
+
+        for (int i = arr.length-1; i>=0; i--) {
+            System.out.print(arr[i] + " ");
+        }
 
     }
 }
