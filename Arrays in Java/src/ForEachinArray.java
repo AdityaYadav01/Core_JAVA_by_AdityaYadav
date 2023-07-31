392e0a4a3c805e20eb5ae6aa9d7222242e08ba6f
Aditya Yadav <Adityayadav44556@gmail.com>
2023-07-31 18:33:53 +0530

    For Each Loop in Array

--- Arrays in Java/src/ForEachinArray.java
+++ Arrays in Java/src/ForEachinArray.java
@@ -1,11 +1,12 @@
 /* For Each Loop - This loop is specifically designed to handle the elements of a collection.
-* Syntax: for(var: collection){
+    Syntax: for(var: collection){
                      statement;
                     }                          */
 
 
 public class ForEachinArray {
     public static void main(String[] args) {
+
 
         /* Write a program to see the use of for-each loop and retrieve the elements
            one by one from an array and display it.*/
