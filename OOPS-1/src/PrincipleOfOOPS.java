fa16660658c1304ef1a6a5f21bb3f841858b2c21
Aditya Yadav <Adityayadav44556@gmail.com>
2023-08-13 20:16:32 +0530

    Principle of OPPs

---
+++ OOPS-1/src/PrincipleOfOOPS.java
@@ -0,0 +1,20 @@
+public class PrincipleOfOOPS {
+
+    /*                                  PRINCIPLE OF OOPs
+    *
+    *
+    *    1. Abstraction - It means hiding the internal details and showing only required things.
+    *       Example - Consider a man driving a car, while driving he will be focusing on steering, gear, accelerator.
+    *                 He doesn't require to know the internal mechanism.
+    *
+    *    2. Encapsulation - It is the process of grouping data in a single section.
+    *        Example - Complete television is a single box where all the mechanism are hidden inside the box all over are capsuled.
+    *
+    *    3. Inheritance - It means designing an object or class by re-using the properties of existing class or object.
+    *                     Inheritance is same as specialization.
+    *         Example - An old style television is transformed with extra features like slim, smart television where it re-used the properties of old television.
+    *
+    *    4. Polymorphism - Polymorphism is the concept where we can perform single operation in different ways.
+    *                      Polymorphism is same as generalization.
+    **/
+}
