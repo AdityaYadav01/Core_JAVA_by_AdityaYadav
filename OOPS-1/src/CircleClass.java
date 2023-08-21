a633eacc9d52a61afe8f9b72f3041145cd4fd739
Aditya Yadav <Adityayadav44556@gmail.com>
2023-08-21 18:37:02 +0530

    Creating a Class for Circle

---
+++ OOPS-1/src/CircleClass.java
@@ -0,0 +1,23 @@
+class Circle{
+    public double radius;
+
+    public double area(){
+        return Math.PI*radius*radius;
+    }
+
+    public double perimeter(){
+        return 2*Math.PI*radius;
+    }
+
+}
+
+public class CircleClass {
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        c1.radius = 5;
+
+        System.out.println("Area: " + c1.area());
+        System.out.println("Perimeter: " + c1.perimeter());
+
+    }
+}
