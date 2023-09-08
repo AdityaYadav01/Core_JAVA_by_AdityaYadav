f0df729cebcc0f61eae0b4763fbb2bb787968703
Aditya Yadav <Adityayadav44556@gmail.com>
2023-09-08 20:29:53 +0530

    Method Overriding in Java

---
+++ OOPS-2/src/MethodOverriding.java
@@ -0,0 +1,7 @@
+// Method Overriding - 1. Redefining the method of super class in subclass.
+// 2. Method will be called depending on the object.
+// 3. Method overriding is achieved in Inheritance.
+
+
+public class MethodOverriding {
+}
