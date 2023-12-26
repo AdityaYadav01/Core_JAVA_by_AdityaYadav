6a6f94e58e62bf8b7e420d8742f35d3ff3de29f3
Aditya Yadav <Adityayadav44556@gmail.com>
2023-12-26 14:20:06 +0530

    Java Practice

---
+++ Practice/src/IntroToJava.java
@@ -0,0 +1,18 @@
+public class IntroToJava {
+    // Public - It is an access modifier which allow us to access the class from anywhere.
+    // class - It is the name group of properties and function.
+    // IntroToJava - It is the name of the class as well as the name of the file.
+    public static void main(String[] args){
+        // static - It is a keyword which allow main method to run without using objects.
+        // void - It is a keyword used when we don't want to return anything from the main method or function.
+        // main - It is the name of method. It is also the entry of java program, and execution start from here.
+        // String[] args - It is a command line argument of string type array.
+
+        System.out.println("Hello! World");
+        // System - It is a final class define in java.lang packages
+        // out - It refers to the standard output stream. It is a variable of PrintStream type which is public and static
+        // member field of the system class.
+        // println - It is the method of PrintStream class, It prints argument passed to it & adds a new line.
+
+    }
+}
